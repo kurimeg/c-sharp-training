618e501cba2f8690cf1203f50f4f62a254bfd4b0
Megumi Kurihara <pomme.c5.musique@gmail.com>
2019-08-02 11:09:14 +0900

    gitignore反映

---
+++ MVCBasic/MVCBasic.Model/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCBasic.Model
+{
+    public class Calculator
+    {
+        public int LeftNum { get; set; }
+
+        public int RightNum { get; set; }
+
+        public string Operator { get; set; }
+    }
+}
