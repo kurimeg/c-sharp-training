3537de4fc957444193d8af5679c80b6d7088890f
Megumi Kurihara <pomme.c5.musique@gmail.com>
2019-08-02 11:22:13 +0900

    ソリューション追加

---
+++ MVCBasic/MVCBasic.WebApp/Models/CalculationViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBasic.WebApp.Models
+{
+    public class CalculationViewModel
+    {
+        public CalculationViewModel()
+        {
+            Operators = new SelectListItem[]{
+                new SelectListItem(){ Value ="＋", Text = "＋"},
+                new SelectListItem(){ Value ="－", Text = "－"},
+                new SelectListItem(){ Value ="×", Text = "×"},
+                new SelectListItem(){ Value ="÷", Text = "÷"},
+            };
+        }
+
+        public string LeftNum { get; set; }
+
+        public string RightNum { get; set; }
+
+        public SelectListItem[] Operators { get; set; }
+        public string SelectedOperator { get; set; }
+    }
+}
